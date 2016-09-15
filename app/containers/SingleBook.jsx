005bffbaf282849b9b60634c5cb40eb19df909c1
electrostaticfleece <trevorwong00@gmail.com>
2016-09-15 11:09:09 -0700

    Updated css for the addBook page

--- app/containers/SingleBook.jsx
+++ app/containers/SingleBook.jsx
@@ -7,6 +7,7 @@ import FloatingCard from 'components/FloatingCard';
 import styles from 'css/components/singleBook';
 import Card from 'components/Card';
 import dynamics from 'dynamics.js';
+import clouds from 'images/clouds.gif';
 
 const cx = classNames.bind(styles)
 
@@ -19,20 +20,22 @@ class SingleBook extends Component {
     const { books: { viewing, search }, changeBook } = this.props;
     const thisBook = viewing.page && viewing.index !== null && search[viewing.page].data ? search[viewing.page].data[viewing.index] : null;
     const bookStyle = {card: true, bookImg: true};
-    const bookBackground = {cardBackground: true, blueRadial: true};
+    const bookBackground = {cardBackground: true, onyx: true};
     return (
       <div>
         { thisBook  ? 
           <span className={cx('layout')}>
-
-            { thisBook.image ?
+            <div className={cx('controller')} >
               <Card cardStyles={{card: bookStyle, background: bookBackground}}> 
-                <FloatingCard image={thisBook.image} dynamics={dynamics}/>
-              </Card> : 
-              null
-            }
+                <FloatingCard image={thisBook.image || clouds} dynamics={dynamics}/>
+              </Card> 
+              <Card cardStyles={{card: {card: true}}} >
+                <button onClick = { (e) => changeBook(-1)} >prev</button>
+                <button onClick = { (e) => changeBook(1)} >next</button>
+              </Card>
+            </div>
             <div className={cx('bookInfo')}>
-              <Card cardStyles={{card: {card: true, title: true}}}>
+              <Card cardStyles={{card: {card: true, ocean: true, title: true},}}>
                 <h2>{thisBook.title}</h2>
               </Card>
               <Card cardStyles={{card: {card:true, content: true}}}>
@@ -40,8 +43,6 @@ class SingleBook extends Component {
                   <p>{thisBook.description}</p>
               </Card>
             </div>
-            <button onClick = { (e) => changeBook(-1)} >prev</button>
-            <button onClick = { (e) => changeBook(1)} >next</button>
           </span>
         : null
         }
